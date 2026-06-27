c0cb1b0e440228faabd990e6d546c92d1429c3b2
skkapilhunters <jindelkapil+skkapil@gmail.com>
2026-06-27 22:26:29 +0530

    Update bot_instance.py

--- bot_instance.py
+++ bot_instance.py
@@ -1,4 +1,3 @@
-
 # In your bot initialization file (e.g., bot_instance.py)
 import os
 import discord
@@ -7,8 +6,11 @@ from discord.ext import commands
 # If you get a proxy URL from a service like Webshare or a free rotation proxy:
 PROXY_URL = os.getenv("PROXY_URL") # e.g., http://username:password@proxy_ip:port
 
+# Define your list of prefixes here
+prefixes = ["!", "?", "$", ";", "%"]
+
 bot = commands.Bot(
-    command_prefix="!", 
-    proxy=PROXY_URL,  # Routes Discord API traffic through a clean IP
+    command_prefix=prefixes,  # Now accepts any of the prefixes in the list
+    proxy=PROXY_URL,          # Routes Discord API traffic through a clean IP
     intents=discord.Intents.default()
 )
